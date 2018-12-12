c017839c2df26c7db5f2e13fd5eda3d7dd006d89
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-12-12 23:28:30 +0000

    Show my Repos

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,13 @@
 // Write code here to communicate with Github
-
+const reposList = document.querySelector("#repos-list");
+const list = document.createElement("li");
+fetch("https://api.github.com/users/behzadkk/repos")
+  .then(respose => respose.json())
+  .then(json => {
+    let myRepos = [];
+    for (let i = 0; i < json.length; i++) {
+      myRepos.push(`<li>${json[i].name}</li>`);
+    }
+    myRepos = myRepos.join("");
+    reposList.innerHTML = `<p>These are my reposetaries on GitHub:</P>${myRepos}`;
+  });
